4dceef768aa9b83be7dccc1e9b3228dea5250300
Mahkam <anifedtochka@mail.ru>
2025-07-26 13:59:22 +0300

    Добавлена логика запросов api

--- enternet-shop/src/api/api.ts
+++ enternet-shop/src/api/api.ts
@@ -1,9 +1,37 @@
-const URL = "https://jsonplaceholder.typicode.com/posts";
+import { Product } from "@/store/store";
 
-async function getData(url: string) {
-  const resp = await fetch(url);
-  const data = await resp.json();
-  console.log(data);
+const API_BASE_URL = "https://fakestoreapi.com";
+
+async function requestData<T>(
+  endpoint: string,
+  options?: RequestInit
+): Promise<T> {
+  const response = await fetch(`${API_BASE_URL}${endpoint}`);
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  return response.json() as Promise<T>;
 }
 
-getData(URL);
+export const api = {
+  getProducts: async (): Promise<Product[]> => {
+    try {
+      return await requestData("/products");
+    } catch (err) {
+      console.error(`Error fetching product`, err);
+      return [];
+    }
+  },
+
+  //получаем продукт по ID
+
+  getProduct: async (id: number): Promise<Product | null> => {
+    try {
+      return await requestData<Product>(`/products/${id}`);
+    } catch (err) {
+      console.error(`Error fetching product ${id}`, err);
+    }
+  },
+};
